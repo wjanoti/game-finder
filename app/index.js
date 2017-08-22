aa993f244e70bbfe1f8e10ffcd0808f4a17ea5a4
William Janoti <wjanoti@gmail.com>
2017-08-22 22:48:38 +0200

    Added fonts, images and basic structure

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
-alert('oi');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import './styles.css';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+ReactDOM.render(
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
